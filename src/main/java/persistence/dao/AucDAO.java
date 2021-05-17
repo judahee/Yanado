cdd783c190a4dcd3b968dad21d749afd6634732b
asdfq <asdfq@DESKTOP-E126RK5>
2021-05-17 21:30:30 +0900

    auc

--- src/main/java/persistence/dao/AucDAO.java
+++ src/main/java/persistence/dao/AucDAO.java
@@ -1,5 +1,36 @@
 package persistence.dao;
 
-public class AucDAO {
+import java.util.List;
 
+public class AucDAO {
+	boolean isParticipate(String userId) {
+		return false;
+	}
+	List<Auc> getAllAucList() {
+		return null;
+	}
+	Auc getAuc(int aucNo) {
+		return null;
+	}
+	void regAuction(Auc auc) {
+	}
+	List<Auc> findAucById(String aucId) {
+		return null;
+	}
+	List<Auc> findAucByProduct(String productId) {
+		return null;
+	}
+	List<Auc> getMyAuc(String userNo) {
+		return null;
+	}
+	List<User> getBuyerListByAucNo(int aucNo) {
+		return null;
+	}
+	void sendAlarm(Alarm alarm) {
+	}
+	void changeStatus(String aucNo) {
+	}
+	Product findProductByAuc(String aucId) {
+		return null;
+	}
 }
