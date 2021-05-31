e7df163beab0cad74a1d0e230b6e7cae0a83478d
asdfq <asdfq@DESKTOP-E126RK5>
2021-05-31 18:59:52 +0900

    auc

---
+++ src/main/java/controller/auc/AucjoinController.java
@@ -0,0 +1,115 @@
+package controller.auc;
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@Controller
+@RequestMapping("/auction")
+public class AucjoinController {
+    private final ReadAuctionService readAuctionService;
+    private final CreateAuctionService createAuctionService;
+    private final DeleteAuctionService deleteAuctionService;
+    private final UpdateAuctionService updateAuctionService;
+    private final UserService userService;
+    private final BidService bidService;
+
+    AuctionController(final ReadAuctionService readAuctionService,
+                      final CreateAuctionService createAuctionService, final DeleteAuctionService deleteAuctionService,
+                      final UpdateAuctionService updateAuctionService, final UserService userService,
+                      final BidService bidService){
+        this.readAuctionService=readAuctionService;
+        this.createAuctionService=createAuctionService;
+        this.deleteAuctionService=deleteAuctionService;
+        this.updateAuctionService=updateAuctionService;
+        this.userService = userService;
+        this.bidService = bidService;
+    }
+
+    @PostMapping("/bid")
+    public ResponseEntity bidAuction(@RequestHeader(value = "Authorization", required = false) String jwtToken, BidDto bidDto) {
+        try{
+            User user = userService.getUserByJwtToken(jwtToken);
+            if(user==null){
+                return new ResponseEntity<>(DefaultRes.UNAUTHORIZATION, HttpStatus.UNAUTHORIZED);
+            }
+            int uid = user.getUid();
+            return new ResponseEntity(bidService.newBid(bidDto, uid), HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+            return new ResponseEntity(DefaultRes.FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/{aid}")
+    public ResponseEntity readAuction(@PathVariable("aid") int aid)
+    {
+        ReadAuctionDto readAuctionDto = new ReadAuctionDto(aid);
+        return new ResponseEntity(readAuctionService.readAuctionByAid(readAuctionDto), HttpStatus.OK);
+    }
+
+    @GetMapping
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    public ResponseEntity readAllAuction(Pageable pageable)
+    {
+        return new ResponseEntity(readAuctionService.readAllAuctions(pageable), HttpStatus.OK);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity readAll(@RequestParam String type, @RequestParam String searchStr, Pageable pageable)
+    {
+        ReadAuctionDto readAuctionDto = new ReadAuctionDto(type, searchStr);
+        return new ResponseEntity(readAuctionService.readSearchByType(readAuctionDto, pageable), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity createAuction(@RequestHeader(value = "Authorization", required = false) String jwtToken, CreateAuctionDto createAuctionDto)
+    {
+        try{
+            User user = userService.getUserByJwtToken(jwtToken);
+            if(user == null)
+            {
+                return new ResponseEntity<>(DefaultRes.UNAUTHORIZATION, HttpStatus.UNAUTHORIZED);
+            }
+            return new ResponseEntity(createAuctionService.newAuction(user, createAuctionDto), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return new ResponseEntity(DefaultRes.FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity deleteAuction(@RequestHeader(value = "Authorization", required = false) String jwtToken, DeleteAuctionDto deleteAuctionDto)
+    {
+        try{
+            User user = userService.getUserByJwtToken(jwtToken);
+            if(user == null)
+            {
+                return new ResponseEntity<>(DefaultRes.UNAUTHORIZATION, HttpStatus.UNAUTHORIZED);
+            }
+            return new ResponseEntity(deleteAuctionService.deleteAuction(user, deleteAuctionDto), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return new ResponseEntity(DefaultRes.FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+    }
+
+    @PutMapping
+    public ResponseEntity updateAuction(@RequestHeader(value = "Authorization", required = false) String jwtToken, UpdateAuctionDto updateAuctionDto)
+    {
+        try{
+            User user = userService.getUserByJwtToken(jwtToken);
+            if(user == null)
+            {
+                return new ResponseEntity<>(DefaultRes.UNAUTHORIZATION, HttpStatus.UNAUTHORIZED);
+            }
+            return new ResponseEntity(updateAuctionService.updateAuction(user, updateAuctionDto), HttpStatus.OK);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return new ResponseEntity(DefaultRes.FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
